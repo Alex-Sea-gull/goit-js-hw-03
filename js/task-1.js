a6be314903480c41da04a6b26691770f9f89e266
Alex Sea <seagullalex81@gmail.com>
2024-06-01 13:43:59 +0300

    add task 1.2

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,15 @@
+function slugify(title) {
+  const strSlugify = title.split(" ").join("-"); // split - з рядка в масив, потім join - з масива в рядок
+  return strSlugify.toLowerCase()
+}
+
+
+
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+
 
 
 
@@ -8,9 +20,3 @@
 // Усі символи slug повинні бути в нижньому регістрі.
 // Усі слова slug повинні бути розділені тире.
 // Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
-
-
-// console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-// console.log(slugify("English for developer")); // "english-for-developer"
-// console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-// console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,19 @@
+function makeArray(firstArray, secondArray, maxLength) {
+    let newArray = firstArray.concat(secondArray);
+    if (newArray.length > maxLength) {
+        newArray.length = maxLength;
+    }
+    return newArray;
+}
 
 
 
-
-
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
 
 
 
@@ -13,11 +24,3 @@
 // В іншому випадку функція повинна повернути весь новий масив.
 
 // Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
-
-
-// console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
-// console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
-// console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
-// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
-// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
-// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
